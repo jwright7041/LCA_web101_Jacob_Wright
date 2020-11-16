19c9cc3fc609100e0c91c7dbc8a392a7e5024ec1
Jacob <jmwright120@gmail.com>
2020-11-16 00:05:39 -0600

    Resume updates

---
+++ myPortfolio/resume.js
@@ -0,0 +1,11 @@
+function nameFade() {
+    document.getElementById("name").className = "fade";
+}
+
+function hide(x) {
+    x.style.opacity = 0;
+}
+
+function show(x){
+    x.style.opacity = 1;
+}
